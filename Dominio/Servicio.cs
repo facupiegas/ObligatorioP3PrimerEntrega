13a453570a38800d8b6ee8a7af265ae4df45aac3
Nicolas Muniz <nicolasmuniz7@gmail.com>
2017-09-09 14:09:49 -0300

    Class: Servicio,Organizador,TipoEvento

---
+++ Dominio/Servicio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Servicio
+    {
+        #region Atributos y Proeperties
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public string Descripcion { get; set; }
+        public List<TipoEvento> Eventos { get; set; }
+
+        #endregion
+        #region Metdos
+        public Servicio(string unNombre,string unaDescripcion) {
+            this.Nombre = unNombre;
+            this.Descripcion = unaDescripcion;
+            this.Imagen = null;
+            this.Eventos = new List<TipoEvento>();
+        }
+        #endregion
+
+    }
+}
---
+++ Dominio/TipoEvento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class TipoEvento
+    {
+        #region Atributos y Properties
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        #endregion
+        #region Metodos
+        public TipoEvento(string unNombre,string unaDescripcion) {
+            this.Nombre = unNombre;
+            this.Descripcion = unaDescripcion;
+        }
+        #endregion
+    }
+}
