d7ee4bcfc41f772d3db27bddbf9b73d72754965b
Facundo <facupiegas@hotmail.com>
2017-09-09 18:38:12 -0300

    Se agrega metodo LIstar Proveedores

--- ConsoleAppObligatorioP3/Program.cs
+++ ConsoleAppObligatorioP3/Program.cs
@@ -11,12 +11,20 @@ namespace ConsoleAppObligatorioP3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("aca estamo en la consola");
+            /*Console.WriteLine("aca estamo en la consola");
             Usuario us = new Usuario("prov","123",Usuario.EnumRol.Proveedor);
             DateTime fecha = new DateTime(2017,11,11);
             Proveedor prov = new Proveedor("210001432188","Ancap","ancap@ancap.com","24090001",fecha,true,us);
-            Console.WriteLine(prov.Guardar());
-            Console.ReadKey();
+            Console.WriteLine(prov.Guardar());*/
+
+            List<Proveedor> lista = Proveedor.ListarProveedores();
+            foreach (Proveedor pTmp in lista)
+            {
+                Console.WriteLine("Nombre Proveedor: " + pTmp.NomFantasia + "\n" + 
+                                  "RUT: " + pTmp.Rut + "\n" +
+                                  "Vip: " + pTmp.Vip);
+            }
+            Console.ReadLine();            
         }
     }
 }
