3049dd3ffcd6d8ab54978655416e726dd982e70f
Nicolas Muniz <nicolasmuniz7@gmail.com>
2017-09-09 13:18:08 -0300

    Add project files.

---
+++ ConsoleAppObligatorioP3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace ConsoleAppObligatorioP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("aca estamo en la consola");
+            Console.ReadKey();
+        }
+    }
+}
