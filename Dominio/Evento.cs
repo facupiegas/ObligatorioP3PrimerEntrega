11c71e0d5be79a1f3057d8ebfc9809d2a45d9bee
Facundo <facupiegas@hotmail.com>
2017-09-09 15:27:35 -0300

    Se crea calse Evento y su constructor Clase Serviciso COntratado queda creada con los atributos comentados

---
+++ Dominio/Evento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Evento
+    {
+        #region Atributos y Properties
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public TipoEvento TipoEvento{ get; set; }
+        public string Direccion{ get; set; }
+        #endregion
+
+        #region Metodos
+        public Evento(String unNombre, DateTime unaFecha, TipoEvento unTipoEvento, string unaDireccion)
+        {
+            this.Nombre = unNombre;
+            this.Fecha = unaFecha;
+            this.TipoEvento = unTipoEvento;
+            this.Direccion = unaDireccion;
+        }
+
+        #endregion
+    }
+}
---
+++ Dominio/ServicioContratado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ServicioContratado
+    {
+        #region Atributos y Properties
+        //public Proveedor Proveedor{ get; set; }
+        public List<Servicio> ListaServicios { get; set; }
+        #endregion
+
+        #region Metodos
+        public ServicioContratado(/*Proveedor unProveedor,*/ List<Servicio> unaListaDeServicios) {
+            //this.Proveedor = unProveedor;
+            this.ListaServicios = unaListaDeServicios;
+        }
+
+        #endregion
+    }
+}
