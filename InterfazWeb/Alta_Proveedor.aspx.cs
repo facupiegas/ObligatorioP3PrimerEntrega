773481b05b126a9183f86004f85ed776002630b3
Facundo <facupiegas@hotmail.com>
2017-09-10 18:25:59 -0300

    Web Form Alta Usuario creado y validado

--- InterfazWeb/Alta_Proveedor.aspx.cs
+++ InterfazWeb/Alta_Proveedor.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
 
 namespace InterfazWeb
 {
@@ -11,7 +12,76 @@ namespace InterfazWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if ((string)Session["tipo"] == "Administrador") //Valido que el usuario se haya logueado y no se saltee la autentificación
+            {
+                Response.Redirect("Login.aspx"); //si no se logueó, lo redirijo a Login
+            }
+        }
 
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //convierto los datos obtenidos de las cajas de texto validados
+            string unNombreUsuario = Convert.ToString(txtNomUsuario.Text);
+            string unaContrasena = Convert.ToString(txtPass.Text);
+            string unRut = Convert.ToString(txtRut.Text);
+            string unNomFantasiaProov = Convert.ToString(txtNomFantasia.Text);
+            string unMailProov = Convert.ToString(txtEmail.Text);
+            string unTelProov = txtTel.Text;
+            bool esVip = false;
+            if(radVipN.Checked || radVipS.Checked)
+            {
+                if(radVipS.Checked)
+                {
+                    esVip = true;
+                }
+                Usuario tmpUser = new Usuario(unNombreUsuario, unaContrasena, Usuario.EnumRol.Proveedor);
+                if (stringEsSoloNumeros(unRut) && stringEsSoloNumeros(unTelProov))
+                {
+                    Proveedor tmpProv = new Proveedor(unRut, unNomFantasiaProov, unMailProov, unTelProov, DateTime.Now.Date, esVip, tmpUser);
+                    int saveUser = tmpUser.Guardar();
+                    int saveProov = tmpProv.Guardar();
+                    if (saveUser != 0 && saveProov != 0){
+                        lblMensaje.Text = "Proveedor Creado con Éxito";
+                        lblMensaje.ForeColor = System.Drawing.Color.Green;
+                        lblMensaje.Visible = true;
+                    }
+                    else if(saveUser == 0)
+                    {
+                        lblMensaje.Text = "(*) El nombre de usuario ingresado ya ha sido utilizado";
+                        lblMensaje.ForeColor = System.Drawing.Color.Red;
+                        lblMensaje.Visible = true;
+                    }
+                    else if (saveProov == 0)
+                    {
+                        lblMensaje.Text = "(*) El proveedor ya existe en el sistema (RUT)";
+                        lblMensaje.ForeColor = System.Drawing.Color.Red;
+                        lblMensaje.Visible = true;
+                    }
+                }
+                else
+                {
+                    lblMensaje.Text = "(*) Rut y/o telefono inválido";
+                    lblMensaje.ForeColor = System.Drawing.Color.Red;
+                    lblMensaje.Visible = true;
+                }
+            }
+            else
+            {
+                lbErrorlVip.Text = "Debe seleccionar una opción";
+                lbErrorlVip.Visible = true;
+            }
+        }
+
+        public bool stringEsSoloNumeros(string unString) //metodo que valida que un string ingresado solo contenga numeros
+        {
+            foreach (char c in unString)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
